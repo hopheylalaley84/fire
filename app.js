fef3fe4c766128681060a2e457ed6ff0a250a527
hopheylalaley84 <ivanov.ap@mail.ru>
2022-10-25 20:16:46 +0300

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var morgan = require('morgan');
 var Api2Pdf = require('api2pdf');
 var mkdirp = require('mkdirp');
 var a2pClient = new Api2Pdf('ed231e48-a5f1-4644-abe1-71e9f22e88dd');
 const download = require('download');
 const PORT = 3000;
-const chalk = require('chalk');
 const { db } = require('./firebase.js');
+var bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+var ip = require("ip");
+console.dir ( ip.address() );
 
 // const createPath = (page) => path.resolve(__dirname,'files', `${page}.html`);
 // http://localhost:3000/file?userId=3&fileId=fileid&fileurl=fileurl
 
-const errorMsg = chalk.bgKeyword('white').redBright;
-const sucessMsg = chalk.bgKeyword('green').white;
+var jsonParser = bodyParser.json();
 
 
-app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`);
-});
 
 async function docConvert(fileUrl) {
     try {
@@ -27,27 +25,15 @@ async function docConvert(fileUrl) {
 
         return fin2['Success'] == true ? fin2['FileUrl'] : 'error';
     } catch (error) {
+        console.log(error);
         return 'error';
     }
 };
 
-async function docDownload(url, userId, fileId) {
-    try {
-        const file = url;
-        const filePath = `${__dirname}/files`;
-        const dirPath = await mkdirp(`${filePath}/${userId}/${fileId}/`);
-        console.log(dirPath);
-        await download(file, dirPath);
-        return 'oky';
-    } catch (error) {
-        console.log(error);
-        return 'err';
-    }
-};
 
 async function fileUpdate(userId, fileId) {
     try {
-        
+
         return 'oky';
     } catch (error) {
         console.log(error);
@@ -56,21 +42,35 @@ async function fileUpdate(userId, fileId) {
 };
 
 
-app.get('/file', async (req, res) => {
+app.get('/upload', async (req, res) => {
 
-    let fileUrl = req.query.fileurl;
-    let userID = req.query.userid;
-    let fileID = req.query.fileid
+    // const file = req.files;
+    console.log(req);
+    console.dir ( ip.address() );
 
-    const res11 = await docConvert(fileUrl);
-    if (res11 != 'error') {
-        const resDownload = await docDownload(res11, `${userID}`, `${fileID}`);
-        resDownload == 'oky' ? res.status(200).send('Download OK') : res.status(500).send('bad1');
-    } else {
-        res.status(500).send('bad2');
-    }
+    res.send('ok');
+
+    // console.log(req.body['FirstName']);
+    // res.json('req.body');
+    //     let fileUrl = req.query.fileurl;
+    //     let userID = req.query.userid;
+    //     let fileID = req.query.fileid
+    //     // let fileUrl1 = 'https://firebasestorage.googleapis.com/v0/b/fireprint-e7649.appspot.com/o/xenNlA24glaqqlms6OJ8VSLcoxx2%2Ffiles%2F%D1%81%D1%87%D0%B5%D1%82.xls?alt=media&token=265965c8-92b4-441b-b244-a480fedcbb46';
+
+
+    //    var  a = fileUrl.replace("---", "&token");
+ß
+    // const res11 = await docConvert('https://firebasestorage.googleapis.com/v0/b/fireprint-e7649.appspot.com/o/xenNlA24glaqqlms6OJ8VSLcoxx2%2Ffiles%2F%D1%81%D1%87%D0%B5%D1%82.xls?alt=media&token=265965c8-92b4-441b-b244-a480fedcbb46');
+    // if (res11 != 'error') {
+    //     const resDownload = await docDownload(res11, `${userID}`, `${fileID}`);
+    //     resDownload == 'oky' ? res.status(200).send('Download OK') : res.status(500).send('bad1');
+    // } else {
+    //     res.status(500).send('bad2');
+    // }
     // console.log(res11);
 
 });
 
-
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
